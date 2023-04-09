129bdb3ae3221106e3e7fc4d926a30364ae04d38
Aleksandr <sky7way@yandex.ru>
2023-04-09 13:12:42 +0300

    24 part

--- app_movie/app.js
+++ app_movie/app.js
@@ -11,8 +11,23 @@ const personalMovieDB = {
 for(let i = 0; i < 2; i++){
     const a = prompt('Один из последних просмотренных фильмов?', ''),
     b = prompt('На сколько оцените его?', '');
-            
-    personalMovieDB.movies[a] = b;
+    if(a != null && b != null && a != '' && b != '' && a.length < 50){
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+
+if(personalMovieDB.count < 10){
+    console.log('Просмотрено довольно мало фильмов');
+} else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+    console.log('Вы классический зритель');
+} else if(personalMovieDB.count >= 30){
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
 }
     
 
